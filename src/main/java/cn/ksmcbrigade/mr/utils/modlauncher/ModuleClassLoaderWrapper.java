8ba4e5e89ef610bc1a647515740522fdbe807961
yiyuyan <179544143@qq.com>
2026-05-03 02:29:35 +0800

    update agent

---
+++ src/main/java/cn/ksmcbrigade/mr/utils/modlauncher/ModuleClassLoaderWrapper.java
@@ -0,0 +1,52 @@
+package cn.ksmcbrigade.mr.utils.modlauncher;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ModuleClassLoaderWrapper {
+
+    private static Object cachedLoader = null;
+
+    private static Object getTransformingClassLoader() {
+        if (cachedLoader != null) {
+            return cachedLoader;
+        }
+
+        Object loader = System.getProperties().getOrDefault("transforming_class_loader", null);
+
+        if (loader != null) {
+            cachedLoader = loader;
+            return cachedLoader;
+        }
+
+        return cachedLoader;
+    }
+
+    //ModuleClassLoaderTransformer
+    public static byte[] maybeTransformClassBytes(final byte[] bytes, final String name, final String context) {
+        Object transformingClassLoader = getTransformingClassLoader();
+
+        if (transformingClassLoader != null) {
+            try {
+
+                Method method = transformingClassLoader.getClass().getDeclaredMethod(
+                        "maybeTransformClassBytes", byte[].class, String.class, String.class);
+                method.setAccessible(true);
+
+                return (byte[]) method.invoke(
+                        transformingClassLoader,
+                        bytes,
+                        name,
+                        context != null ? context : "classloading"
+                );
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                System.out.println("[Agent] error in wrapper class loader for class: "+name);
+                e.printStackTrace();
+                return bytes;
+            }
+        } else {
+            System.out.println("[Agent] Cannot find TransformingClassLoader, class: "+ name);
+            return bytes;
+        }
+    }
+}
